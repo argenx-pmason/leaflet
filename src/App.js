efe865966fc4b88915419169e5e43af4310fd754
pmason <pmason@argenx.com>
2024-09-23 07:40:29 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import { Icon } from "leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
 
 function App() {
+  const [position, setPosition] = useState([51.05361111, 3.72527778]),
+  argenx=[51.00941, 3.72382]
+// TODO: add geojson to add chloropleth map
+// https://fmuchembi.medium.com/let-us-build-a-choropleth-map-using-react-leaflet-together-3245d30ac900
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // <div className="App">
+    <Box>
+      <MapContainer
+        center={position}
+        zoom={9}
+        scrollWheelZoom={true}
+        style={{ height: "100vh" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={argenx}
+          icon={
+            new Icon({
+              iconUrl: markerIconPng,
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </Box>
+    // </div>
   );
 }
 
