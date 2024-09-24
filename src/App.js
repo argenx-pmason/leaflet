317d4b5d803ef33c6b139204384d8fdcc4107a00
philipmason <phil@woodstreet.org.uk>
2024-09-24 07:18:52 +0100

    geojson

--- src/App.js
+++ src/App.js
@@ -5,13 +5,44 @@ import markerIconPng from "leaflet/dist/images/marker-icon.png";
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
+import geojsonFeature from "./geojsonFeature.json";
 
 function App() {
   const [position, setPosition] = useState([51.05361111, 3.72527778]),
-  argenx=[51.00941, 3.72382]
-// TODO: add geojson to add chloropleth map
-// https://fmuchembi.medium.com/let-us-build-a-choropleth-map-using-react-leaflet-together-3245d30ac900
+    argenx = [51.00941, 3.72382],
+    getColor = (d) => {
+      return d === "BEL"
+        ? "#800026"
+        : d === "FRA"
+        ? "#BD0026"
+        : d > 200
+        ? "#E31A1C"
+        : d > 100
+        ? "#FC4E2A"
+        : d > 50
+        ? "#FD8D3C"
+        : d > 20
+        ? "#FEB24C"
+        : d > 10
+        ? "#FED976"
+        : "#FFEDA0";
+    },
+    style = (feature) => {
+      return {
+        fillColor: getColor(feature.properties.ISO3_CODE),
+        weight: 2,
+        opacity: 1,
+        color: "white",
+        dashArray: "3",
+        fillOpacity: 0.7,
+      };
+    };
+  // TODO: add geojson to add chloropleth map
+  // https://fmuchembi.medium.com/let-us-build-a-choropleth-map-using-react-leaflet-together-3245d30ac900
 
+  useEffect(() => {
+    // L.geoJSON(geojsonFeature).addTo(map);
+  }, []);
   return (
     // <div className="App">
     <Box>
@@ -24,6 +55,11 @@ function App() {
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <GeoJSON
+          attribution="&copy; credits due..."
+          data={geojsonFeature}
+          style={style}
         />
         <Marker
           position={argenx}
